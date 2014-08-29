838e781c2153d797e39fa6c986108c4490585358
Jonathan Dade <jonathandade@gmail.com>
2014-08-29 11:51:03 +0100

    Added custom-503 and pricing extras

---
+++ Fastly API/bash/pricing_extras.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+# Pricing_Extras.sh
+# =================
+# This script contains examples of using the Fastly API to create, list, update, delete directors.
+# They presume some familiarity with Fastly and APIs in general and should be used as examples only.
+#
+# Additional details on using this script can be found at http://docs.fastly.com.
+#
+
+if [ "$FASTLY_API_DEBUG" == "true" ]; then
+	set -e
+	set -x
+fi
+
+# Load the configuration file that contains the api key and service id.
+source ~/.fastly/api-creds.sh
+
+# Define a function to display the output nicely.
+pretty-out() {
+	echo
+	echo "$1"
+}
+
+# Set up a variable to save re-typing the whole lot for each API call.
+FASTLY_API_URL="https://api.fastly.com/service/$FASTLY_SERVICE_ID"
+VERSION="2"
+FASTLY_API_URL="$FASTLY_API_URL/version/$VERSION"
+
+# The rest of the commands assume you have either got the correct configuration
+# version via the API or by accessing https://app.fastly.com/
+
+# get the customer id
+ID=`curl -X GET "$FASTLY_API_KEY" "https://api.fastly.com/current_customer" | sed -e 's/.*\"id\":\"\([A-Za-z0-9][A-Za-z0-9]*\).*/\1/'`
+echo "ID: $ID"
