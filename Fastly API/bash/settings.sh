7e56bfc377c4597de9a92f66f8842d2d1824c704
Jonathan Dade <jonathandade@gmail.com>
2015-01-30 13:19:04 +0000

    Added some more testing scripts

--- Fastly API/bash/settings.sh
+++ Fastly API/bash/settings.sh
@@ -24,11 +24,27 @@ pretty-out() {
 
 # Set up a variable to save re-typing the whole lot for each API call.
 FASTLY_API_URL="https://api.fastly.com/service/$FASTLY_SERVICE_ID"
-VERSION="4"
+VERSION="24"
 FASTLY_API_URL="$FASTLY_API_URL/version/$VERSION"
 
 # The rest of the commands assume you have either got the correct configuration
 # version via the API or by accessing https://app.fastly.com/
 
 # First lets get the current values
+pretty-out "Current service settings: "
+curl -X GET -H "$FASTLY_API_KEY" "$FASTLY_API_URL/settings"
+
+# Now let's update those values
+pretty-out "Setting defaults:"
+curl -X PUT -sv -H "$FASTLY_API_KEY" \
+  -H "Content-type: application/json" \
+  "$FASTLY_API_URL/settings" -d \
+'{
+  "general.default_host":"www.test.com",
+  "general.default_ttl":"1",
+  "general.default_pci":"0"
+}'
+
+# Now let's see those changes
+pretty-out "New values:"
 curl -X GET -H "$FASTLY_API_KEY" "$FASTLY_API_URL/settings"
