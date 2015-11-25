d1749aedf515d7fd62561c20d627265eac92a3bf
Jonathan Dade <jonthandade@gmail.com>
2015-11-25 08:05:30 +0000

    yup bucket empty shiz

--- AWS/s3-bucket-empty.rb
+++ AWS/s3-bucket-empty.rb
@@ -6,17 +6,18 @@
 
 require 'AWS-SDK'
 require '~/.aws/keys'
+require 'pry'
 
 # Set up the AWS access credentials and set some defaults.
 AWS.config({:access_key_id => ACCESS_KEY_ID, :secret_access_key => SECRET_ACCESS_KEY, :region => 'eu-west-1', :use_ssl => :true, })
-
+binding.pry
 s3 = AWS::S3.new()
 
 s3.buckets.each do |bucket|
 	puts bucket.name
 end
 
-s3.buckets['jdade-htlogs'].objects.each do |obj|
+s3.buckets['rightmove-logs'].objects.each do |obj|
 	p "Deleting #{obj.key}"
 	p "..."
 	p "#{obj.delete}"
