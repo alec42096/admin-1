1148ba72350efe2bee296ba30c590b899b191688
Jonathan Dade <jonathandade@gmail.com>
2016-06-12 15:33:06 +0100

    update to site-test

--- Tools/site-test.rb
+++ Tools/site-test.rb
@@ -1,26 +1,28 @@
 require "httpclient"
 require "syslog/logger"
 
-// set up basic for requests
+# set up basic for requests
 url = 'http://www.inde-test.jdade.me.uk/'
 c = HTTPClient.new
 
-// set up logging
-results = ['200':0, '300':0, '400':0, '500':0, hits:0]
-log = Syslog.logger.new('monitoring')
+# set up logging
+results = {twos:0, threes:0, fours:0, fives:0, hits:0}
+log = Syslog::Logger.new('monitor')
 
 while true do
 
     start = Time.now
     r = c.get url
     if r.status >= 100 && r.status < 300
-        results[:'200'] += 1
+        results[:twos] += 1
     elsif r.status >= 300 && r.status < 400
-        results[:'300'] += 1
+        results[:threes] += 1
     elsif r.status >= 400 && r.status < 500
-        results[:'400'] += 1
+        results[:fours] += 1
+    elsif r.status >= 500 && r.status < 600
+        results[:fives] += 1
     end
     results[:hits] += 1 if /HIT/.match(r.headers['X-Cache'])
-    log.info "2xx = "+results[:'200']+", 3xx = "+results[:'300']+", 4xx = "+results[:'400']+", 5xx = "+results[:'500']
-    Thread.sleep(Time.now + 0.5 - start)
+    log.info "2xx = "+results[:twos].to_s+", 3xx = "+results[:threes].to_s+", 4xx = "+results[:fours].to_s+", 5xx = "+results[:fives].to_s
+    sleep(Time.now + 0.5 - start)
 end
